591cc4e732339943036c9fa653d3a5ca7817381a
SBHObject <168382040+SBHObject@users.noreply.github.com>
2024-10-23 15:00:13 +0900

    Add: 플레이어 스텟관련 스크립트 추가
    체력, 스테미나, 배고픔 스텟 추가

---
+++ SBH_SurvivalPractice/Assets/Scripts/PlayerControlls/PlayerCondition.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerCondition : MonoBehaviour, IDamageable
+{
+    public UICondition uiCondition;
+
+    private Condition health { get { return uiCondition.health; } }
+    private Condition hunger { get { return uiCondition.hunger; } }
+    private Condition stamina { get {  return uiCondition.stamina; } }
+
+    public float noHungerHealthDecay;
+    public UnityAction onTakeDamage;
+
+    private void Update()
+    {
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if(hunger.curValue < 0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        if(health.curValue <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Eat(float amount)
+    {
+        hunger.Add(amount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("플레이어 사망");
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        health.Subtract(damageAmount);
+        onTakeDamage?.Invoke();
+    }
+}
