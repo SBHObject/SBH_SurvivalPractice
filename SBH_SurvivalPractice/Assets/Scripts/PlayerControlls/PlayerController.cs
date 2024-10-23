77e045553b255f5a4dbd6be09ce6f37d2e89c9c8
SBHObject <168382040+SBHObject@users.noreply.github.com>
2024-10-23 14:30:17 +0900

    Update: 캐릭터 움직임 추가 구현

--- SBH_SurvivalPractice/Assets/Scripts/PlayerControlls/PlayerController.cs
+++ SBH_SurvivalPractice/Assets/Scripts/PlayerControlls/PlayerController.cs
@@ -39,12 +39,18 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Move();
+    }
+
+    private void LateUpdate()
+    {
         if (canLook)
         {
-            
+            CamLook();
         }
     }
 
+    //움직임 입력
     public void OnMove(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
@@ -55,5 +61,77 @@ public class PlayerController : MonoBehaviour
         {
             curMovementInput = Vector2.zero;
         }
+    }
+
+    //점프 입력
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Started)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    //마우스 위치 입력(시점용)
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    //캐릭터 움직이기
+    public void Move()
+    {
+        //입력으로부터 움직일 방향 결정
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+
+        //방향 * 속도 -> 실제 움직임
+        dir *= moveSpeed;
+
+        //y축 : 점프, 현재 velocity를 가져와서 높이 유지
+        dir.y = rb.velocity.y;
+
+        //velocity 로 움직임 구현
+        rb.velocity = dir;
+    }
+
+    //카메라 위,아래로 돌리기
+    private void CamLook()
+    {
+        camCurXRot += mouseDelta.y * lookSensitivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minLook, maxLook);
+
+        camContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+    }
+
+    //캐릭터가 땅에 닿았는지 확인
+    private bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down)
+        };
+
+        for(int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //커서 조작, true일때 커서조작 가능
+    public void ToggleCursor(bool toggle)
+    {
+        Cursor.lockState = toggle? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = toggle;
+        canLook = !toggle;
     }
 }
