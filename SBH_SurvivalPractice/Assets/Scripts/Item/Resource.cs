6596e09fb56afd73c470f2ea993451bee53aceab
SBHObject <168382040+SBHObject@users.noreply.github.com>
2024-10-24 16:35:21 +0900

    Add: Item 자원기능 추가
    자원 획득 가능 아이템으로 자원 오브젝트 공격시 아이템 드랍 시스템 추가

--- SBH_SurvivalPractice/Assets/Scripts/Item/Resource.cs
+++ SBH_SurvivalPractice/Assets/Scripts/Item/Resource.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Resource : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ItemData itemToDrop;
+    public int quantityPerHit;
+    public int capacity;
 
-    // Update is called once per frame
-    void Update()
+    public void Gather(Vector3 hitPoint, Vector3 hitNormal)
     {
-        
+        for (int i = 0; i < quantityPerHit; i++)
+        {
+            if (capacity <= 0)
+            {
+                break;
+            }
+
+            capacity -= 1;
+
+            Instantiate(itemToDrop.dropPrefab, hitPoint + Vector3.up * 0.2f, Quaternion.LookRotation(hitNormal, Vector3.up));
+        }
     }
 }
