3d7afed962f6e3f01151330b85bf24d2cab5b7fb
SBHObject <168382040+SBHObject@users.noreply.github.com>
2024-10-24 11:21:50 +0900

    Add: 아이템 일부, 아이템 상호작용 기능 추가

--- SBH_SurvivalPractice/Assets/Scripts/PlayerControlls/Player.cs
+++ SBH_SurvivalPractice/Assets/Scripts/PlayerControlls/Player.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
     public PlayerController controller;
     public PlayerCondition condition;
+
+    public ItemData itemData;
+    public UnityAction addItem;
 
     private void Awake()
     {
