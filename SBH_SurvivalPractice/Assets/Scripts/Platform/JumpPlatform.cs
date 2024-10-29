85f40e3060871421c2ea7ae198eccbd11f1294e5
SBHObject <168382040+SBHObject@users.noreply.github.com>
2024-10-29 14:52:08 +0900

    Update: JumpPlatform 발사대에 효과음, 이펙트 추가

--- SBH_SurvivalPractice/Assets/Scripts/Platform/JumpPlatform.cs
+++ SBH_SurvivalPractice/Assets/Scripts/Platform/JumpPlatform.cs
@@ -8,6 +8,18 @@ public class JumpPlatform : MonoBehaviour, IInteractable
     private float launchTimer = 1f;
     [SerializeField]
     private float launchPower = 1000f;
+    private AudioSource audioSource;
+
+    public GameObject launchEffect;
+    public Transform launchPos;
+
+    private Collider thisCollider;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        thisCollider = GetComponent<Collider>();
+    }
 
     public string GetInteractPrompt()
     {
@@ -25,9 +37,16 @@ public class JumpPlatform : MonoBehaviour, IInteractable
 
     private IEnumerator Launch()
     {
+        thisCollider.enabled = false;
         yield return new WaitForSeconds(launchTimer);
+        audioSource.PlayOneShot(audioSource.clip);
+        GameObject effect = Instantiate(launchEffect, launchPos.position, Quaternion.identity);
+        Destroy(effect, 2f);
 
         CharacterManager.Instance.Player.controller.canMove = true;
         CharacterManager.Instance.Player.controller.LaunchPlayer(launchPower, launchPosition.up);
+
+        yield return new WaitForSeconds(launchTimer);
+        thisCollider.enabled = true;
     }
 }
